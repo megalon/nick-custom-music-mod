e58cd499474ec2e9577fc5d3c710ba6e12fb9bb9
Megalon <megalongaming@gmail.com>
2021-10-06 02:33:49 -0700

    Stage and menu IDs

---
+++ Utils/Consts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NickCustomMusicMod.Utils
+{
+    public static class Consts
+    {
+        public static readonly string[] StageIDs = {
+            "SlideHouse",
+            "Shanty",
+            "Rooftop",
+            "Trash",
+            "Loud",
+            "CarnivalLofi",
+            "SewersCombined",
+            "Armada",
+            "Drome",
+            "YMC",
+            "House",
+            "Stomp",
+            "Temple2",
+            "Waterfall",
+            "DuoKitchen",
+            "Zone",
+            "DuoMadness",
+            "SpiritWorld",
+            "Omashu",
+            "Playground"
+        };
+
+        public static readonly string[] MenuIDs = { 
+            "MainMenu",
+            "Versus",
+            "OnlineMenu",
+            "ArcadeMap",
+            "Victory",
+            "LoseV1",
+            "LoseV2"
+        };
+    }
+}
