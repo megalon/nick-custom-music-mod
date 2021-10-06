3f622c1117f3ab690d9b9b287fce557556ea4944
Megalon <megalongaming@gmail.com>
2021-10-06 15:11:37 -0700

    Load from any subdirs in main dirs

--- Management/CustomMusicManager.cs
+++ Management/CustomMusicManager.cs
@@ -13,36 +13,65 @@ namespace NickCustomMusicMod.Management
 {
     internal class CustomMusicManager
 	{
-        public static void Init()
+		public static string rootCustomSongsPath;
+
+		public static void Init()
         {
 			songDictionaries = new Dictionary<string, Dictionary<string, MusicItem>>();
 
+			rootCustomSongsPath = Path.Combine(Paths.BepInExRootPath, "CustomSongs");
+
 			Task.Run(delegate ()
 			{
-				foreach (string stageID in Consts.StageIDs)
-				{
-					LoadSongsFromFolder("Stages", stageID);
-				}
+				// Create the folder if it doesn't exist
+				Directory.CreateDirectory(rootCustomSongsPath);
+
+				// Generate folders, incase any were deleted
 				foreach (string menuID in Consts.MenuIDs)
 				{
-					LoadSongsFromFolder("Menus", menuID);
+					Directory.CreateDirectory(Path.Combine(rootCustomSongsPath, "Menus", menuID));
 				}
+
+				foreach (string stageID in Consts.StageIDs)
+				{
+					Directory.CreateDirectory(Path.Combine(rootCustomSongsPath, "Stages", stageID));
+				}
+
+				// Load songs
+				LoadFromSubDirectories("Stages");
+				LoadFromSubDirectories("Menus");
 			});
         }
 
+		public static void LoadFromSubDirectories(string parentFolderName)
+		{
+			var subDirectories = Directory.GetDirectories(Path.Combine(rootCustomSongsPath, parentFolderName));
+
+			foreach (string directory in subDirectories)
+			{
+				var directoryName = Path.GetFileName(directory);
+
+				Plugin.LogDebug($"directory {directoryName} full path {directory}");
+				LoadSongsFromFolder(parentFolderName, directoryName);
+			}
+		}
+
 		public static void LoadSongsFromFolder(string parentFolderName, string folderName)
 		{
-			string path = Path.Combine(Paths.BepInExRootPath, Path.Combine("CustomSongs", parentFolderName, folderName));
-			Directory.CreateDirectory(path);
+			Plugin.LogDebug($"LoadSongsFromFolder parentFolderName:{parentFolderName} folderName:{folderName}");
+			
+			string path = Path.Combine(rootCustomSongsPath, parentFolderName, folderName);
+
 			Dictionary<string, MusicItem> musicItemDict = new Dictionary<string, MusicItem>();
+
 			foreach (string text in from x in Directory.GetFiles(path)
 				where x.ToLower().EndsWith(".ogg") || x.ToLower().EndsWith(".wav") || x.ToLower().EndsWith(".mp3")
 				select x)
 				{
 				string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(text);
 
-				Console.WriteLine($"Loaded {text}");
-					
+				Plugin.LogInfo($"Found custom song: {parentFolderName}\\{folderName}\\{Path.GetFileName(text)}");
+
 				MusicItem music = new MusicItem
 				{
 					id = "CUSTOM_" + fileNameWithoutExtension,
