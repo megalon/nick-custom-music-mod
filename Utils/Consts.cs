630918ad7394ce690807e8983416f63acb5c8241
Megalon <megalongaming@gmail.com>
2021-10-06 15:27:20 -0700

    Test commit on new url

--- Utils/Consts.cs
+++ Utils/Consts.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace NickCustomMusicMod.Utils
 {
+    // These should really be loaded from the game, not from a stored list, but
+    // this list simply dictates which folders are auto generated.
+    // Currently the mod will also load files from any extra subfolder in the "Stage" or "Menu" folders,
+    // incase extra stages are added in the future.
     public static class Consts
     {
         public static readonly string[] StageIDs = {
