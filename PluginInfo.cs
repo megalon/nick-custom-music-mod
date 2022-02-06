ee7a4e91def5f5b3a46ff2c46222b36bbad256db
Megalon <megalongaming@gmail.com>
2022-02-05 21:43:38 -0800

    1.5.2 Version bump

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -8,6 +8,6 @@ namespace NickCustomMusicMod
     {
         public const string PLUGIN_GUID = "megalon.nick_custom_music_mod";
         public const string PLUGIN_NAME = "NickCustomMusicMod";
-        public const string PLUGIN_VERSION = "1.5.1";
+        public const string PLUGIN_VERSION = "1.5.2";
     }
 }
